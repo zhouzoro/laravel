08fce7c3774d4a348cea4bfad913860e3bd6a3c1
Jason Zhou <zyol@outlook.com>
2016-05-25 01:45:38 +0800

    gulpfile advanced

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,102 @@ var sourcemaps = require('gulp-sourcemaps');
 var livereload = require('gulp-livereload');
 var del = require('del');
 var babel = require("gulp-babel");
+var jade = require('gulp-jade');
+ 
+function renderJade(evt) {
+    if (evt.type == 'changed') {
+        return gulp.src(evt.path)
+            .pipe(jade({
+                pretty: true
+            }))
+            .pipe(gulp.dest('./wwwroot'))
+            .pipe(notify({
+                message: 'Jade task complete: ' + evt.path.substring(evt.path.lastIndexOf('\\'))
+            }))
+    }
+}
+
+function renderJadeToBlade(evt) {
+    if (evt.type == 'changed') {
+        return gulp.src(evt.path)
+            .pipe(jade({
+                pretty: true
+            }))
+            .pipe(rename(function(path){
+                path.extname='.blade.php'
+            }))
+            .pipe(gulp.dest(evt.path.substring(0, evt.path.lastIndexOf('\\'))))
+            .pipe(notify({
+                message: 'Jade2Blade complete: ' + evt.path.substring(evt.path.lastIndexOf('\\'))
+            }))
+    }
+}
+
+function renderSass(evt) {
+    if (evt.type == 'changed') {
+        return sass(evt.path, {
+                style: 'expanded'
+            })
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest(evt.path.substring(0, evt.path.lastIndexOf('\\')) + '\\dist'))
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(cssnano())
+            .pipe(gulp.dest(evt.path.substring(0, evt.path.lastIndexOf('\\')) + '\\dist'))
+            .pipe(notify({
+                message: 'Sass task complete: ' + evt.path.substring(evt.path.lastIndexOf('\\'))
+            }))
+    }
+}
+
+function renderJS(evt) {
+    if (evt.type == 'changed') {
+        return gulp.src(evt.path)
+            .pipe(sourcemaps.init())
+            .pipe(rename({
+                suffix: '.babeled'
+            }))
+            .pipe(babel())
+            .pipe(gulp.dest(evt.path.substring(0, evt.path.lastIndexOf('\\')) + '\\dist'))
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(uglify())
+            .pipe(sourcemaps.write(evt.path.substring(0, evt.path.lastIndexOf('\\')) + '\\maps'))
+            .pipe(gulp.dest(evt.path.substring(0, evt.path.lastIndexOf('\\')) + '\\dist'))
+            .pipe(notify({
+                message: 'Babel task complete: ' + evt.path.substring(evt.path.lastIndexOf('\\'))
+            }))
+    }
+}
+
+function renderIMG(evt) {
+    if (evt.type == 'changed') {
+        return gulp.src(evt.path)
+            .pipe(cache(imagemin({
+                optimizationLevel: 3,
+                progressive: true,
+                interlaced: true
+            })))
+            .pipe(gulp.dest(evt.path.substring(0, evt.path.lastIndexOf('\\'))))
+            .pipe(notify({
+                message: 'Image task complete: ' + evt.path.substring(evt.path.lastIndexOf('\\'))
+            }))
+    }
+}
+gulp.task('templates', function() {
+  var YOUR_LOCALS = {};
+ var locations =[components]
+  gulp.src('resources/views/lib/*.jade')
+    .pipe(jade({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('./dist/'))
+});
 
 gulp.task('default', ['clean'], function() {
     gulp.start('styles', 'scripts', 'images');
@@ -80,17 +176,20 @@ gulp.task('images', function() {
             message: 'Images task complete'
         }));
 });
+
 gulp.task('clean', function() {
     return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
 });
 
 gulp.task('watch', function() {
+    // Watch .jade files
+    gulp.watch('resources/views/**/*.jade', renderJadeToBlade);
 
     // Watch .sass files
-    gulp.watch('wwwroot/stylesheets/*.sass', ['styles']);
+    gulp.watch('wwwroot/stylesheets/*.sass', renderSass);
 
     // Watch .js files
-    gulp.watch('wwwroot/javascripts/*.js', ['scripts']);
+    gulp.watch('wwwroot/javascripts/*.js', renderJS);
 
     // Watch image files
     //gulp.watch('wwwroot/images/*', ['images']);
